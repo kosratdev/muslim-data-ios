180ba71d92edab1b3554e5d8dea56bb93323c601
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2018-10-11 23:29:02 +0300

    feat: Add names of allah in the library

---
+++ MuslimData/Classes/Names/Name.swift
@@ -0,0 +1,16 @@
+//
+//  Name.swift
+//  MuslimData
+//
+//  Created by Kosrat D. Ahmad on 10/11/18.
+//
+
+import Foundation
+
+/// Name medel that represents a name with it's translation.
+public struct Name {
+    // MARK: - Properties
+
+    let name: String
+    let translated: String
+}
