b5340f73f383933206e9efab2d26ccb73cb1f8e6
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2018-10-13 23:27:42 +0300

    test: Add tests for azkar categories

---
+++ Example/Tests/AzkarTests.swift
@@ -0,0 +1,58 @@
+//
+//  AzkarTests.swift
+//  MuslimData_Tests
+//
+//  Created by Kosrat D. Ahmad on 10/13/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import MuslimData
+
+class AzkarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAzkarCategories() {
+        // Test English azkar categories
+        Azkars.azkarCategories(language: .en) { categories, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(categories)
+            XCTAssertEqual(categories!.count, 11)
+        }
+
+        // Test Arabic azkar categories
+        Azkars.azkarCategories(language: .ar) { categories, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(categories)
+            XCTAssertEqual(categories!.count, 11)
+        }
+
+        // Test Central Kurdish azkar categories
+        Azkars.azkarCategories(language: .ckb) { categories, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(categories)
+            XCTAssertEqual(categories!.count, 11)
+        }
+
+        // Test Farsi azkar categories
+        Azkars.azkarCategories(language: .fa) { categories, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(categories)
+            XCTAssertEqual(categories!.count, 11)
+        }
+
+        // Test Russian azkar categories
+        Azkars.azkarCategories(language: .ru) { categories, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(categories)
+            XCTAssertEqual(categories!.count, 11)
+        }
+    }
+}
