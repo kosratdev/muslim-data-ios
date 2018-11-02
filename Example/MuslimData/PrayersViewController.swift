d9d1bb2909500ad7664ffc50bac694a5cf67e8f9
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2018-11-02 17:29:48 +0300

    feat: Add prayer times screen for demo app

---
+++ Example/MuslimData/PrayersViewController.swift
@@ -0,0 +1,73 @@
+//
+//  PrayersViewController.swift
+//  MuslimData_Example
+//
+//  Created by Kosrat D. Ahmad on 11/2/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import MuslimData
+
+class PrayersViewController: UIViewController {
+    // MARK: - Outlets
+
+    @IBOutlet weak var locationDetail: UILabel!
+    @IBOutlet weak var prayerTable: UITableView!
+
+    // MARK: - Properties
+
+    var prayerTimes: [String] = []
+    let prayerNames = ["Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha"]
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        prayerTable.dataSource = self
+        prayerTable.tableFooterView  = UIView()
+
+        // Get prayer times
+        getPrayers()
+    }
+
+    // MARK: - Helper Methods
+
+    /// Get prayer times from the MuslimData library
+    func getPrayers() {
+        let location = Location.loadSavedLocation()
+        let attributes = PrayerAttribute(method: .makkah, asrMethod: .shafii, adjustAngle: .angleBased)
+        PrayerTime.getPrayerTimes(location: location, date: Date(),
+                                  attributes: attributes) { (prayerTime, error) in
+            guard error == nil else {
+                print("Prayer tims didn't found for the specified properties.")
+                return
+            }
+            self.prayerTimes = prayerTime!.formatPrayers(.time12)
+            self.prayerTable.reloadData()
+        }
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMM yyyy"
+        let stringDate = dateFormatter.string(from: Date())
+
+        locationDetail.text = "\(location.city), \(location.countryName)\n\(stringDate)"
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension PrayersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prayerTimes.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "prayerCell", for: indexPath)
+        cell.textLabel?.text = prayerNames[indexPath.row]
+        cell.detailTextLabel?.text = prayerTimes[indexPath.row]
+
+        return cell
+    }
+}
